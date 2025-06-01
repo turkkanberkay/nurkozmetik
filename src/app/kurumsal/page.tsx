2e585c2fcc07c39f4c54ef99617128ea209a33ab
Berkay Türkkan <turkkanberkay@gmail.com>
2025-06-01 13:45:09 +0300

    kurumsal added

---
+++ src/app/kurumsal/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+import "../../styles/wordings.css";
+
+const KurumsalPage = () => (
+  <div className="w-full bg-[#f0f4ff] text-[#333] p-8 min-h-screen">
+    <div className="max-w-5xl mx-auto px-4 sm:px-6 py-4">
+      <section className="mb-8">
+        <h2 className="title">Firma Hakkında</h2>
+        <p>
+          Nur Kozmetik 30 yılı aşkın üretim ve ihracat tecrübesine sahiptir ve yıllardır kozmetik ve sağlık sektörünün ihtiyacı olduğu ürünleri yüksek standart ve kalite ile piyasaya sunmaktadır. Firmamız ilk olarak şampuan, saç kremi ve jöle üretimine başlamış, geçtiğimiz yıllarda dünyadaki kozmetik sektöründeki gelişmeleri ve trendleri takip ederek ürün skalasını genişletmiştir. Bu süreçte Ar-Ge çalışmalarını da geliştirerek üretimine tüketici ihtiyaçları doğrultusunda yeni ürünler ilave etmiş, 2010 yılında Tıbbi Cihaz bölümünü açarak bu alanda da üretime başlamıştır. Şirketimiz GMP, ISO 9001:2008, ISO 13485:2003 kalite sistemlerine uygun olarak üretim yapmaktadır.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="subtitle">ARGE Faaliyetlerimiz</h2>
+        <p>
+          ARGE çalışmaları bizi diğer firmalardan ayıran faktördür. Yenilikçi ve hedefine ulaştıran ürünler üretmek bizim temel işimizdir. Firmamız Ar-Ge ve inovasyon faaliyetlerine önem verir, hammaddelerin üretime alınmasından ürünün tüketiciye sunulmasına kadar geçen süreç takip edilmekte bütün test ve analizleri yapılarak yüksek kalitede ürün ve hizmet sunulmaktadır.
+        </p>
+        <p className="mt-2">
+          Arge faaliyetleri, gerek Türkiye’de yürürlükte olan kanun ve yönetmeliklere gerekse Avrupa Birliği’nin yürürlüğe koymuş olduğu standart ve yönetmeliklere uygun olarak yürütülmektedir. Laboratuvarlarımızda kendi markalarımıza ait ürünlerin yanında, birçok ulusal ve uluslararası firma için private label ya da fason üretim için ARGE çalışmaları yapılmaktadır.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="subtitle">Fason Üretim Faaliyetlerimiz</h2>
+        <p>
+          Firmamız Tıbbi Cihaz Bölümü ve Kozmetik bölümünde Private-Label (fason) üretim yapmaktadır. Sektörde uzun yıllar var olmanın getirdiği bilgi ve tecrübe birikimi ile müşterilerimize en iyi kalite ve fiyatta ürünler üretmekteyiz. Uzman ekibimizle birlikte her müşterimize özel proje, ürün ve formül geliştirme, ambalaj, üretim ve yasalara uygunluk konusunda hizmet vermekteyiz.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="subtitle">Değerlerimiz</h2>
+        <p>
+          Nur Kozmetik bünyesinde tüm çalışanlarımız kendilerini en iyi şekilde geliştirebilecekleri ve değerli hissedebilecekleri bir çalışma ortamı sunmaya çalışıyoruz. Hedefimiz sorumluluk duygusu ile hareket eden, etik değerlere bağlı, ekip çalışmasına uygun, firmamızı ve kendisini bir adım öteye taşıyacak yüksek performanslı kişilerli şirketimiz bünyesinde katmak, bu kişilerin gelişimlerini sağlamak ve şirketimiz bünyesinde tutmak yönünde politikalar gerçekleştirmek. Çalışanlarımızın sahip olduğu ve dikkat ettiğimiz değerler;
+        </p>
+      </section>
+    </div>
+  </div>
+);
+
+export default KurumsalPage;
